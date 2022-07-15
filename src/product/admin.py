f8b28e458225a3ae20012b45cf5c53d7ec2005bf
AhasanulWorkstation <ahasanular@gmail.com>
2022-07-15 12:21:26 +0600

    nothing

--- src/product/admin.py
+++ src/product/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Product, ProductVariant, ProductVariantPrice, ProductImage, Variant
+
+
+class ProductAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(ProductVariant)
+admin.site.register(ProductVariantPrice)
+admin.site.register(Variant)
 
 # Register your models here.
--- src/product/views/product.py
+++ src/product/views/product.py
@@ -1,6 +1,6 @@
 from django.views import generic
 
-from product.models import Variant
+from product.models import Product, ProductVariant, ProductVariantPrice, Variant
 
 
 class CreateProductView(generic.TemplateView):
@@ -11,4 +11,19 @@ class CreateProductView(generic.TemplateView):
         variants = Variant.objects.filter(active=True).values('id', 'title')
         context['product'] = True
         context['variants'] = list(variants.all())
+        return context
+
+
+class ListProductView(generic.ListView):
+    template_name = 'products/list.html'
+
+    def get_queryset(self):
+        queryset = Product.objects.filter().all()
+        print("DS")
+        print(queryset)
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['product'] = True
         return context
